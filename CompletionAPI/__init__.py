cfcc51894c490d47a55e7a18b3160e54f9b51c25
Dhruviika <E21CSEU0292@bennett.edu.in>
2023-06-05 15:12:44 +0530

    Default api variable

--- CompletionAPI/__init__.py
+++ CompletionAPI/__init__.py
@@ -2,7 +2,7 @@ import logging
 import openai
 import azure.functions as func
 
-openai.api_key = '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'
+openai.api_key = 'your_api_key'
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
