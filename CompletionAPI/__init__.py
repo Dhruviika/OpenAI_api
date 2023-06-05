69b4662c62528b80dcd9329a7e0d936e99decb4d
Dhruviika <E21CSEU0292@bennett.edu.in>
2023-06-05 15:05:28 +0530

    project added.

---
+++ CompletionAPI/__init__.py
@@ -0,0 +1,24 @@
+import logging
+import openai
+import azure.functions as func
+
+openai.api_key = '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'
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+    
+
+    req_body = req.get_json()
+    logging.info(type(req_body))
+
+    output = openai.Completion.create(
+        model = req_body['model'],
+        prompt = req_body['prompt'],
+        max_tokens = req_body['max_tokens'],
+        temperature = req_body['temperature']
+    )
+
+    output_text = output['choices'][0]['text']
+   
+    return func.HttpResponse(output_text,status_code=200)
+        
